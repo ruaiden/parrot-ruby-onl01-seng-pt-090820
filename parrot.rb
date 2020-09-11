897126cde480faccf3618ec5257485d370fe10d8
Aiden Marques <aiden.marques94@gmail.com>
2020-09-11 00:32:32 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (phrase = "Squawk!")
-  puts "#{phrase}"
-end
+#def parrot (phrase = "Squawk!")
+# puts "#{phrase}"
+#end
 
 def parrot (phrase = "Squawk!")
   return "#{phrase}"
+  puts "#{phrase}"
 end
